b781ffc50e0e5919d08c81989ae926d9283875d9
David Gerrells <dgerrells@hotmail.com>
2022-05-05 01:14:09 +0000

    app wrappers

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,30 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import { ThemeProvider } from "@mui/material/styles";
+import CssBaseline from "@mui/material/CssBaseline";
+import theme, { darkTheme } from "../src/theme";
+import { Box } from "@mui/material";
+import { Navbar } from "../src/components/Navbar";
+import { useState } from "react";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  const [isDarkMode, setIsDarkMode] = useState(false);
+
+  return (
+    <>
+      <Head>
+        <meta name="viewport" content="initial-scale=1, width=device-width" />
+      </Head>
+      <ThemeProvider theme={isDarkMode ? darkTheme : theme}>
+        <CssBaseline />
+        <Navbar toggleTheme={() => setIsDarkMode(!isDarkMode)} />
+        <Box mt={[2, 4]} mx={[2, "auto"]} maxWidth={768}>
+          <Component {...pageProps} />
+        </Box>
+      </ThemeProvider>
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
